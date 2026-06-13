147fcc164e17ebd81c6f32efadd632d06ba7c4e5
vhugo23 <vhugodossantos23@gmail.com>
2026-06-13 11:20:24 -0500

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/cafocolo_api/CafocoloApiApplication.java
@@ -0,0 +1,13 @@
+package com.cafocolo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafocoloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafocoloApiApplication.class, args);
+	}
+
+}
